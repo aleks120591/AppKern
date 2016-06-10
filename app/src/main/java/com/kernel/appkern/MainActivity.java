572cf996b728d78642f2d1fe422d87d55d4bb925
aleks120591@gmail.com <0p8i6y4r2w>
2016-06-10 11:10:03 +0400

    MainActivity and BaseActivity

---
+++ app/src/main/java/com/kernel/appkern/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kernel.appkern;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Warlock on 10.06.2016.
+ */
+public class MainActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
