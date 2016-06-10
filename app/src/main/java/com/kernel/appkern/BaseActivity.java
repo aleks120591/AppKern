c71d4a71b88785cdab30f21b5887897bff92c23d
aleks120591@gmail.com <0p8i6y4r2w>
2016-06-10 12:06:16 +0400

    header_main

--- app/src/main/java/com/kernel/appkern/BaseActivity.java
+++ app/src/main/java/com/kernel/appkern/BaseActivity.java
@@ -1,16 +1,30 @@
 package com.kernel.appkern;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 /**
  * Created by Warlock on 10.06.2016.
  */
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (getParentViewId()!=0)setContentView(getParentViewId());
+        else
+            if (getParentView()!=null)setContentView(getParentView());
+
+        Toolbar toolbar = getToolbar();
+        toolbar.setPopupTheme(R.style.ThemeOverlay_AppCompat_Dark);
+
+
     }
+
+    public abstract int getParentViewId();
+    public abstract View getParentView();
+    public abstract Toolbar getToolbar();
 }
--- app/src/main/java/com/kernel/appkern/MainActivity.java
+++ app/src/main/java/com/kernel/appkern/MainActivity.java
@@ -2,14 +2,33 @@ package com.kernel.appkern;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 /**
  * Created by Warlock on 10.06.2016.
  */
 public class MainActivity extends BaseActivity{
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public int getParentViewId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public View getParentView() {
+        return null;
+    }
+
+    @Override
+    public Toolbar getToolbar(){
+        return toolbar = (Toolbar)findViewById(R.id.toolbar);
     }
 }
